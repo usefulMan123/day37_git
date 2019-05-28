23a34fd390e2bd6c7143281b71824d5ee4457f2f
usefulMan123 <2337441196@qq.com>
2019-05-28 11:57:18 +0800

    Update Add.java

--- src/com/qfedu/test/Add.java
+++ src/com/qfedu/test/Add.java
@@ -5,7 +5,7 @@ public class Add {
     public static void main(String[] args) {
 
         System.out.println("hahahh");
-        System.out.println("Today is a fine day");
+       // System.out.println("Today is a fine day");
         //System.out.println("xixixi");
     }
 
