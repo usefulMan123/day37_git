12b307572b63edde24062d61fd8d194ad8e53c1c
dong <2337441196@qq.com>
2019-05-28 11:09:44 +0800

    update add.java

--- src/com/qfedu/test/Add.java
+++ src/com/qfedu/test/Add.java
@@ -3,6 +3,11 @@ package com.qfedu.test;
 public class Add {
 
     public static void main(String[] args) {
+
         System.out.println("hahahh");
+        System.out.println("Today is a fine day");
     }
+
+
 }
+
