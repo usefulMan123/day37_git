a0ccecb718be755b5aa2b0f7f7ed35fe4313550c
dong <2337441196@qq.com>
2019-05-28 14:31:25 +0800

    add info

--- src/com/qfedu/test/Add.java
+++ src/com/qfedu/test/Add.java
@@ -4,9 +4,10 @@ public class Add {
 
     public static void main(String[] args) {
 
-        System.out.println("hahahh");
+        System.out.println("afjhafd");
        // System.out.println("Today is a fine day");
         //System.out.println("xixixi");
+
         int i = 0;
     }
 
